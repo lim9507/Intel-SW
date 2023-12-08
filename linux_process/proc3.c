a4a93b571d53007b47c0bea02205b5c02451c356
Lim <149364859+lim9507@users.noreply.github.com>
2023-12-08 09:20:11 +0900

    Add files via upload

---
+++ linux_process/proc3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+void timeout(int sig)
+{
+	if(sig == SIGALRM) puts ("Time out!!");
+	alarm(2);
+}
+
+int main()
+{
+	struct sigaction act;
+	act.sa_handler = timeout;			//  kernel registers the function to call
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGALRM, &act, 0);
+	alarm(2);
+
+	for(int i = 0; i < 3; i++){
+		puts ("wait...");
+		sleep(10);
+	}
+
+	return 0;
+}
